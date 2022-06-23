eab40ba3559600f737d3a5499dcd46b569b11654
José Augusto <jaaugusto.n@gmail.com>
2022-06-23 05:33:59 -0300

    Adiciona filtros

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,17 +2,72 @@ import React, { useContext } from 'react';
 import { StarWarsContext } from '../context/StarWarsProvider';
 
 export default function Filters() {
-  const { setFilterByName } = useContext(StarWarsContext);
+  const { setFilterByName,
+    setFilterByColumn,
+    setFilterByComparison,
+    setFilterByValue,
+    filterByColumn,
+    filterByComparison,
+    filterByValue,
+    setHandleClick } = useContext(StarWarsContext);
 
-  const handleChange = ({ target: { value } }) => {
-    setFilterByName(value);
+  const handleChange = ({ target }) => {
+    if (target.type === 'text') setFilterByName(target.value);
+    else setFilterByValue(target.value);
+  };
+
+  const handleSelection = ({ target }) => {
+    if (target.id === 'column-filter') setFilterByColumn(target.value);
+    else setFilterByComparison(target.value);
+  };
+
+  const handleClick = () => {
+    setHandleClick(true);
   };
 
   return (
-    <input
-      type="text"
-      data-testid="name-filter"
-      onChange={ (e) => handleChange(e) }
-    />
+    <form>
+      <input
+        type="text"
+        data-testid="name-filter"
+        onChange={ (e) => handleChange(e) }
+      />
+      <select
+        id="column-filter"
+        data-testid="column-filter"
+        onChange={ (e) => handleSelection(e) }
+        value={ filterByColumn }
+      >
+        <option>population</option>
+        <option>orbital_period</option>
+        <option>diameter</option>
+        <option>rotation_period</option>
+        <option>surface_water</option>
+      </select>
+      <select
+        id="comparison-filter"
+        data-testid="comparison-filter"
+        onChange={ (e) => handleSelection(e) }
+        value={ filterByComparison }
+      >
+        <option>maior que</option>
+        <option>igual a</option>
+        <option>menor que</option>
+      </select>
+      <input
+        id="value-filter"
+        type="number"
+        data-testid="value-filter"
+        onChange={ (e) => handleChange(e) }
+        value={ filterByValue }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ () => handleClick() }
+      >
+        Filtrar
+      </button>
+    </form>
   );
 }
--- src/context/StarWarsProvider.jsx
+++ src/context/StarWarsProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import getPlanetsData from '../services/searchAPI';
+// import response from '../testData';
 
 export const StarWarsContext = createContext();
 
@@ -8,19 +9,46 @@ const StarWarsProvider = ({ children }) => {
   const [data, setData] = useState([]);
   const [filterByName, setFilterByName] = useState('');
   const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [filterByColumn, setFilterByColumn] = useState('population');
+  const [filterByComparison, setFilterByComparison] = useState('maior que');
+  const [filterByValue, setFilterByValue] = useState(0);
+  const [handleClick, setHandleClick] = useState(false);
 
   const fetchPlanets = async () => {
     const planetsData = await getPlanetsData();
+    // const planetsData = response.results;
     setData(planetsData);
   };
 
   useEffect(() => {
     fetchPlanets();
-    const filteredByName = data.filter((planet) => planet.name
-      .toLowerCase().includes(filterByName))
-      .sort((a, b) => a.name.localeCompare(b.name));
-    setFilteredPlanets(filteredByName);
-  }, [filterByName, data]);
+    // console.log(filterByComparison, filterByColumn, filterByValue);
+    if (handleClick) {
+      const filteredByName = data.filter((planet) => {
+        if (filterByComparison === 'maior que') {
+          return planet.name
+            .toLowerCase()
+            .includes(filterByName) && planet[filterByColumn] > parseFloat(filterByValue);
+        } if (filterByComparison === 'menor que') {
+          return planet.name
+            .toLowerCase()
+            .includes(filterByName) && planet[filterByColumn] < parseFloat(filterByValue);
+        }
+        if (filterByComparison === 'igual a') {
+          return planet.name
+            .toLowerCase()
+            .includes(filterByName)
+            && parseFloat(planet[filterByColumn]) === parseFloat(filterByValue);
+        } return null;
+      });
+      setFilteredPlanets(filteredByName);
+    } else {
+      const filteredByName = data.filter((planet) => planet.name
+        .toLowerCase().includes(filterByName));
+      setFilteredPlanets(filteredByName);
+    }
+  }, [filterByName,
+    handleClick, filterByColumn, filterByComparison, filterByValue, data]);
 
   const initialState = {
     data,
@@ -28,6 +56,14 @@ const StarWarsProvider = ({ children }) => {
     filterByName,
     setFilterByName,
     filteredPlanets,
+    filterByColumn,
+    setFilterByColumn,
+    filterByComparison,
+    setFilterByComparison,
+    filterByValue,
+    setFilterByValue,
+    handleClick,
+    setHandleClick,
   };
 
   return (
